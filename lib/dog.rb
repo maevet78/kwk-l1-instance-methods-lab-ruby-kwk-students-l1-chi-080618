f53021ae357d7b0711a4ae7aef4c7b9a8420d38e
maevet78 <190439mt@saintviator.com>
2018-08-10 16:25:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # dog.rb
+
+class dog
+  def bark
+    puts "Woof!"
+  end 
+  
+  def sit
+    puts "The dog is sitting"
+  end
+end 
+
+
+  
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,9 @@
 # person.rb
+class Person
+  def talk
+    puts "Hello Wolrd!"
+  end
+  def walk
+    puts "The peron is walking"
+  end
+end   
